51496218009b3a36043ee1649672774584ac6db6
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-06-21 21:58:38 +0430

    Changes added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post("create-post",[PostController::class, "create"]);
+Route::get("Post",[PostController::class, "index"]);
+Route::view("Post/create", 'Post.create');
+Route::put('Post/update/{id}', [PostController::class, 'update']);
+Route::get('Post/edit/{id}', [PostController::class, 'edit']);
